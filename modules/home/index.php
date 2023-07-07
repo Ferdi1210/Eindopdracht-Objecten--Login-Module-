b682a462336ed8774918afc37ad540dd4cfba5ad
Ferdi <cumenferdi@gmail.com>
2023-07-07 09:33:11 +0200

    Full Upload P10

---
+++ modules/home/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    return '<div class="content">Welkom bij <strong><span style="color: red">FC Rode Rakketen!</span></strong></div>
+            <p>De club waar je 7 dagen per week kunt voetballen!</p>
+            <p>Wil jij nou voetballen bij een van de beste voetbalveringen in Hoogeveen? Kom dan bij ons voetballen!</p>
+    
+    <div class="logomid">
+      <img class="logomid" src="http://schoolwerk.nl/FCRodeRaketten/FcLogo.PNG">
+    </div>
+
+    <style>
+        .content {
+            margin-top: 20px;
+        }
+    </style>';
+?> 
